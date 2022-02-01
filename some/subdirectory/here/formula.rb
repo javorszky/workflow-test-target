b9d04386e6f7b6c247bd3dae9fed8b59ad755b66
javorszky <javorszky@users.noreply.github.com>
2022-02-01 15:46:48 +0000

    Update homebrew formula to version v0.3.2

--- some/subdirectory/here/formula.rb
+++ some/subdirectory/here/formula.rb
@@ -2,9 +2,9 @@ class Subo < Formula
   desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
   homepage "https://suborbital.dev"
   head "https://github.com/suborbital/subo.git"
-  url "https://github.com/suborbital/subo/archive/v0.3.0.tar.gz"
-  sha256 "85c53369904cd83393e6adb9fb29f1737c81ebae669459aa572da1c5f03a69ea"
-  version "0.3.0"
+  url "https://github.com/suborbital/subo/archive/v0.3.2.tar.gz"
+  sha256 "<<<sha256>>>"
+  version "0.3.2"
 
   depends_on "go" => :build
 
