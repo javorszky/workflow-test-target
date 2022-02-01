0edf0380b08cb10326f718beb4d4365d2f6be4ed
Gabor Javorszky <gabor@suborbital.dev>
2022-02-01 15:15:45 +0000

    Added original file

---
+++ some/subdirectory/here/formula.rb
@@ -0,0 +1,19 @@
+class Subo < Formula
+    desc "The Suborbital CLI for building and deploying WebAssembly-based projects."
+    homepage "https://suborbital.dev"
+    head "https://github.com/suborbital/subo.git"
+    url "https://github.com/suborbital/subo/archive/v0.3.0.tar.gz"
+    sha256 "85c53369904cd83393e6adb9fb29f1737c81ebae669459aa572da1c5f03a69ea"
+    version "0.3.0"
+  
+    depends_on "go" => :build
+  
+    def install
+      ENV["GOPATH"] = HOMEBREW_CACHE/"go_cache"
+      system "go", "build", "-o", bin/"subo"
+    end
+  
+    test do
+      system "#{bin}/subo --version"
+    end
+  end
